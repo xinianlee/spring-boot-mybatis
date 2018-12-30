882c486876d828958e6f1ee9770ac70952bd1469
xinianlee <xinianlee@qq.com>
2018-12-30 20:08:46 +0800

    spring-data

---
+++ spring-boot-data/src/main/java/com/xinian/springbootdata/SpringBootDataApplication.java
@@ -0,0 +1,14 @@
+package com.xinian.springbootdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataApplication.class, args);
+	}
+
+}
+
